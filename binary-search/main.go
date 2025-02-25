190bf99d571521d895f220b8309af744b45dff86
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-25 15:30:24 -0600

    feat:binary search recursive
    se agrego la variante recursiva

--- binary-search/main.go
+++ binary-search/main.go
@@ -1,35 +1,48 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	my_list := []int{0, 2, 4, 6, 8}
-	num, err := binarySearch(1, my_list)
-	if err != nil {
-		fmt.Println("None")
-		return
-	}
-
+	my_list := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	num := binarySearch(my_list, 3)
+	res, count := binarySearchRecursive(my_list, 15)
 	fmt.Println(num)
-
+	fmt.Println(res, count)
 }
 
-func binarySearch(item int, list []int) (int, error) {
+func binarySearch(list []int, num int) int {
 	low := 0
 	high := len(list) - 1
 	for low <= high {
-		mid := (low + high)
+		mid := (low + high) / 2
 		guess := list[mid]
-		if guess == item {
-			return mid, nil
-		} else if guess > item {
+		switch {
+		case guess == num:
+			return mid
+		case guess > num:
 			high = mid - 1
-		} else {
+		default:
 			low = mid + 1
 		}
 	}
-	return 0, errors.New("None")
+	return -1
+}
+
+func binarySearchRecursive(list []int, num int) (result, count int) {
+	mid := len(list) / 2
+	switch {
+	case len(list) == 0:
+		result = -1
+	case list[mid] > num:
+		result, count = binarySearchRecursive(list[:mid], num)
+	case list[mid] < num:
+		result, count = binarySearchRecursive(list[mid:], num)
+		if result >= 0 {
+			result += mid + 1
+		}
+	default:
+		result = mid
+	}
+	count++
+	return
 }
